f51768545697b22a4a12cc08c4aca33d4c3f30be
Nikita Ivanov <53362543+uberfella@users.noreply.github.com>
2019-12-09 14:22:46 +0900

    Create RomanToArabicConverter.java

---
+++ RomanToArabicConverter.java
@@ -0,0 +1,33 @@
+package calc;
+
+import java.util.HashMap;
+
+public class RomanToArabicConverter {
+    
+    private static int[] base_numbers = {100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static HashMap<Integer, String> map = new HashMap<Integer, String>();
+
+    private static void setup() {
+        map.put(1, "I");
+        map.put(4, "IV");
+        map.put(5, "V");
+        map.put(9, "IX");
+        map.put(10, "X");
+        map.put(40, "XL");
+        map.put(50, "L");
+        map.put(90, "XC");
+        map.put(100, "C");
+    }
+
+    public String intToRoman(int num) {
+        setup();
+        String result = new String();
+        for (int i : base_numbers) {
+            while (num >= i) {
+                result += map.get(i);
+                num -= i;
+            }
+        }
+        return result;
+    }
+}
